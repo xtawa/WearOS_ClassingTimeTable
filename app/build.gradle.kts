62c3804379beb5180ded907463e7ec928bbd8136
Xihao Zhang <zeromostia@gmail.com>
2026-07-20 20:26:24 +0800

    Fix receiver and tile main-thread blocking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,6 +143,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
 
     testImplementation("junit:junit:4.13.2")
