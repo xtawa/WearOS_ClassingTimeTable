50c0c6d8c7883d8578a20aa7463bdc13bd8c86f3
Xihao Zhang <zeromostia@gmail.com>
2026-08-04 20:00:22 +0800

    Enable release shrinking

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -80,7 +80,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             if (hasReleaseSigning) {
                 signingConfig = signingConfigs.getByName("release")
             }
